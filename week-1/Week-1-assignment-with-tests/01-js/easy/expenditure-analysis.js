96a885db1c8cbf72388b6d65d4ac20a04c13cd6e
Shubham Bonde <shubhambonde2000@gmail.com>
2024-01-20 22:33:11 +0530

    Solved expenditure-analysis

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -8,8 +8,63 @@
   - `npm run test-expenditure-analysis`
 */
 
+// function calculateTotalSpentByCategory(transactions) {
+  
+//   try {
+//     if (transactions.length <= 0) return [];
+//   } catch(err) {
+//     throw new Error(err);
+//   }
+
+//   let categoryMap = new Map();
+
+//   let categoryWiseExpense = transactions.reduce((accumulator, transaction) => {
+//     if (accumulator.get(transaction.category) === undefined) {
+//       accumulator.set(transaction.category,  0);
+//     }
+
+//     accumulator.set(transaction.category, categoryMap.get(transaction.category)+transaction.price);
+
+//     return accumulator;
+//   }, categoryMap);
+
+//   let catWiseExpenseList = [];
+
+    
+//   categoryWiseExpense.forEach((totalPrice, category, map) => {
+//     let categoryObj = {};
+//     categoryObj["category"] = category;
+//     categoryObj["totalSpent"] = totalPrice;
+//     catWiseExpenseList.push(categoryObj);
+//   });
+  
+
+//   return catWiseExpenseList;
+// }
+// Cleaner solution
+
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  let expensePerCategory = {}
+
+  // iterate over all transactions data
+  transactions.forEach(transaction => {
+    const {category, price} = transaction;
+
+    if (!expensePerCategory[category]) {
+      expensePerCategory[category] = price;
+    } else {
+      expensePerCategory[category] += price;
+    }
+  })
+
+  let categoryWiseExpenses = Object.keys(expensePerCategory).map(categoryName =>  ({
+    category: categoryName,
+    totalSpent: expensePerCategory[categoryName]
+  }));
+  
+  return categoryWiseExpenses;
 }
+
 
 module.exports = calculateTotalSpentByCategory;
